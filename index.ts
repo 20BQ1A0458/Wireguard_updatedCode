9a74c51da9fdcbe9b5c80f9afb85fe3632f7883b
Bhargav Ram <163117308+20BQ1A0458@users.noreply.github.com>
2024-12-23 14:17:14 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -112,21 +112,20 @@
 // });
 
 
-const dgram = require("dgram");
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
 
-const server = dgram.createSocket("udp4");
-
-server.on("message", (msg, rinfo) => {
-console.log(`Received message: ${msg} from ${rinfo.address}:${rinfo.port}`);
+// Handling incoming messages
+server.on('message', (msg, rinfo) => {
+  console.log(`Received message: ${msg} from ${rinfo.address}:${rinfo.port}`);
 });
 
-server.on("listening", () => {
-const address = server.address();
-console.log(`Server listening on ${address.address}:${address.port}`);
+// Server listening on port 51820 (NodePort)
+server.on('listening', () => {
+  const address = server.address();
+  console.log(`Server listening on ${address.address}:${address.port}`);
 });
 
-//to node port 30001
-server.bind(30001);
-
-
+// Bind the server to the port 51820 (the NodePort)
+server.bind(51820);
 
