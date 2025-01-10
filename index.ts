07ec420823a1e6e7e9303014f7c6dedc407d6854
ManiQC5390 <admin.pqc@qclairvoyance.in>
2025-01-10 10:45:02 +0530

    index file changes

--- index.ts
+++ index.ts
@@ -31,7 +31,9 @@ const getPodNameByIndex = async (index: number): Promise<string> => {
     "kubectl get pods -n wireguard -o jsonpath='{.items[*].metadata.name}'"
   );
 
-  const podNames = output.split(" ").filter((name) => name.startsWith("node-wireguard"));
+  const podNames = output
+    .split(" ")
+    .filter((name) => name.startsWith("node-wireguard"));
 
   if (index < 0 || index >= podNames.length) {
     throw new Error("Index out of range for WireGuard pods");
@@ -43,8 +45,7 @@ const getPodNameByIndex = async (index: number): Promise<string> => {
 // Add Peer to Kubernetes Pod
 const addPeerWithKubernetes = async (
   clientPublicKey: string,
-  assignedIP: string,
-  podName: string
+  assignedIP: string
 ): Promise<void> => {
   try {
     // Add the peer to the WireGuard configuration in the Kubernetes pod
@@ -55,18 +56,49 @@ const addPeerWithKubernetes = async (
     const command2 = `wg-quick save wg0`;
     await executeCommand(command2);
   } catch (error) {
-    console.error("Error in addPeerWithKubernetes:", error instanceof Error ? error.message : error);
+    console.error(
+      "Error in addPeerWithKubernetes:",
+      error instanceof Error ? error.message : error
+    );
+    throw error;
+  }
+};
+
+// Add Peer to Kubernetes Pod
+const removePeerWithKubernetes = async (
+  clientPublicKey: string,
+  podName: string
+): Promise<void> => {
+  try {
+    // Add the peer to the WireGuard configuration in the Kubernetes pod
+    const cmdRemovePeer = `kubectl exec ${podName} -- wg set wg0 peer ${clientPublicKey} remove`;
+    await executeCommand(cmdRemovePeer);
+
+    // Save the WireGuard configuration
+    const cmdSaveWGConfig = `wg-quick save wg0`;
+    await executeCommand(cmdSaveWGConfig);
+  } catch (error) {
+    console.error(
+      "Error in removingPeerWithKubernetes:",
+      error instanceof Error ? error.message : error
+    );
     throw error;
   }
 };
 
 // Add Peer to Local WireGuard Instance
-const addPeer = async (clientPublicKey: string, assignedIP: string): Promise<void> => {
+const addPeer = async (
+  clientPublicKey: string,
+  assignedIP: string
+): Promise<void> => {
   try {
     const command = `wg set wg0 peer ${clientPublicKey} allowed-ips ${assignedIP}/32`;
     await executeCommand(command);
   } catch (error) {
-    console.error("Error in addPeer:", error instanceof Error ? error.message : error);
+    console.error(
+      "Error in addPeer:",
+      error instanceof Error ? error.message : error
+    );
     throw error;
   }
 };
@@ -90,26 +122,132 @@ app.post("/add-peer", async (req: Request, res: Response): Promise<any> => {
 
     // Randomly select one of the WireGuard pods (assuming 2 pods)
     const randomIndex = Math.floor(Math.random() * 2); // Random index (0 or 1)
-    const podName = await getPodNameByIndex(randomIndex);
+    //const podName = await getPodNameByIndex(randomIndex);
 
     // Add the peer to the selected Kubernetes pod
-    await addPeerWithKubernetes(clientPublicKey, assignedIP, podName);
+    await addPeerWithKubernetes(clientPublicKey, assignedIP);
 
     // Retrieve the server's public key from the selected pod
     const serverPublicKey = await executeCommand(
       `cat /etc/wireguard/publickey`
     );
 
+    const nodePort = await getNodePort();
+    const externalIP = await getExternalIP();
+    const podName = await getPodName();
     // Respond with success
     res.status(200).json({
       message: "Peer added successfully",
       assignedIP,
       podName,
-      randomIndex,
+      nodePort,
+      externalIP,
       serverPublicKey: serverPublicKey.trim(),
     });
   } catch (error) {
-    res.status(500).json({ error: error instanceof Error ? error.message : "An error occurred" });
+    res.status(500).json({
+      error: error instanceof Error ? error.message : "An error occurred",
+    });
+    console.error("Add Peer Error:", error);
+  }
+});
+
+function getPodName() {
+  return executeCommand("echo $POD_NAME");
+}
+
+function getNodePort() {
+  return executeCommand(
+    //here we are hardcoding the service name, you can pass it as an argument
+    "kubectl get svc wireguard -o=jsonpath='{.spec.ports[0].nodePort}'"
+  );
+}
+
+function getHostName() {
+  return executeCommand("hostname");
+}
+
+function getExternalIP() {
+  var nodeName = getHostName();
+  return executeCommand(
+    `kubectl get node ${nodeName} -o jsonpath='{.items[0].status.addresses[?(@.type==\"ExternalIP\")].address}'`
+  );
+}
+
+// Endpoint for adding a new peer
+app.post("/add-peer", async (req: Request, res: Response): Promise<any> => {
+  const { clientPublicKey, podName } = req.body;
+
+  // Validate clientPublicKey
+  if (!clientPublicKey) {
+    return res.status(400).json({ error: "clientPublicKey is required" });
+  }
+
+  try {
+    // Assign an IP from the pool for the new peer
+    const assignedIP = poolManager.assignIP(clientPublicKey);
+
+    if (assignedIP === null) {
+      return res.status(500).json({ error: "No available IPs" });
+    }
+
+    // Randomly select one of the WireGuard pods (assuming 2 pods)
+    const randomIndex = Math.floor(Math.random() * 2); // Random index (0 or 1)
+    //const podName = await getPodNameByIndex(randomIndex);
+
+    // Add the peer to the selected Kubernetes pod
+    await addPeerWithKubernetes(clientPublicKey, assignedIP);
+
+    // Retrieve the server's public key from the selected pod
+    const serverPublicKey = await executeCommand(
+      `cat /etc/wireguard/publickey`
+    );
+
+    const nodePort = await getNodePort();
+    const externalIP = await getExternalIP();
+    const podName = await getPodName();
+    // Respond with success
+    res.status(200).json({
+      message: "Peer added successfully",
+      assignedIP,
+      podName,
+      nodePort,
+      externalIP,
+      serverPublicKey: serverPublicKey.trim(),
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error instanceof Error ? error.message : "An error occurred",
+    });
+    console.error("Add Peer Error:", error);
+  }
+});
+
+app.post("/remove-peer", async (req: Request, res: Response): Promise<any> => {
+  const { clientPublicKey, podName } = req.body;
+
+  // Validate clientPublicKey
+  if (!clientPublicKey) {
+    return res.status(400).json({ error: "clientPublicKey is required" });
+  }
+
+  // Validate podName
+  if (!podName) {
+    return res.status(400).json({ error: "podName is required" });
+  }
+
+  try {
+    // Add the peer to the selected Kubernetes pod
+    await removePeerWithKubernetes(req.body.clientPublicKey, req.body.podName);
+
+    // Respond with success
+    res.status(200).json({
+      message: "Peer removed successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error instanceof Error ? error.message : "An error occurred",
+    });
     console.error("Add Peer Error:", error);
   }
 });
@@ -119,34 +257,7 @@ app.listen(4000, () => {
   console.log("Server is running on http://0.0.0.0:4000");
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // ------------------------------------------------------Socket Code ---------------------------Working Fine--------------------------------------------
-
-
-
-
-
-
-
 
 // import dgram, { RemoteInfo } from 'dgram';
 
@@ -177,4 +288,3 @@ app.listen(4000, () => {
 // server.bind(PORT, HOST, () => {
 //   console.log(`Server bound to ${HOST}:${PORT}`);
 // });
-
